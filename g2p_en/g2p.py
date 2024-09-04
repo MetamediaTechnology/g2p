e18beabb1cd5b9212b7bf38f9885354f4427cee2
uttanon <uttanon.aug@gmail.com>
2024-09-04 12:10:33 +0700

    test push partial word *add preflight check*

--- g2p_en/g2p.py
+++ g2p_en/g2p.py
@@ -185,9 +185,9 @@ class G2p(object):
                 pron = self.cmu[word][0]
             elif word.split("-")[0] in self.cmu:
                 #partial word
-                wg = word.split("-")
-                for w in wg:
-                    pron+=self.cmu[w][0]
+                for w in word.split("-"):
+                    if w in self.cmu:
+                        pron+=self.cmu[w][0]
             elif predictOOV: # predict for oov
                 pron = self.predict(word)
             else:
